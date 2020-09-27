9fb4a058ea496258d719c1ab3070022d6a5ec0fe
Stefano Marcolini <39852020+stefanomarcolini@users.noreply.github.com>
2020-09-27 19:00:09 +0200

    Update ItemRepositoryTest.java

--- unit-test-rest/src/test/java/com/unit/test/rest/unittestrest/data/ItemRepositoryTest.java
+++ unit-test-rest/src/test/java/com/unit/test/rest/unittestrest/data/ItemRepositoryTest.java
@@ -13,7 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.unit.test.rest.unittestrest.annotaions.UnitTest;
+import com.unit.test.rest.unittestrest.annotations.UnitTest;
 import com.unit.test.rest.unittestrest.model.Item;
 
 @RunWith(SpringRunner.class)
